8e10b555461d3c7f3c8657fe7ea7ef6448fc07af
BrendanR81 <brendan.w.richards-1@ou.edu>
2025-10-22 15:33:41 -0500

    Starter skeleton for LinkedList

--- src/finalProject/LinkedList.java
+++ src/finalProject/LinkedList.java
@@ -1,5 +1,53 @@
 package finalProject;
 
-public class LinkedList {
-
+public class LinkedList<T> {
+	
+	private int size;
+	private Node<T> head;
+	
+	public int size()
+	{
+		return 0;
+	}
+	
+	public void add(Node<T> node)
+	{
+		
+	}
+	
+	public void remove()
+	{
+		
+	}
+	
+	public T get(int index)
+	{
+		return new T;
+	}
+	
+	public void clear()
+	{
+		
+	}
+	
+	
+	
+	
+	
+	private static class Node<T>
+	{
+		private T value;
+		private Node<T> next;
+		
+		public Node(T value)
+		{
+			this.value = value;
+		}
+		
+		public T getValue()
+		{
+			return value;
+		}
+	}
 }
+
