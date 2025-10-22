3abdbfe0a35e530ef8cd7b580739047c60092481
phun0008 <phungthuydangthu0706@gmail.com>
2025-10-22 15:57:21 -0500

    methods

--- src/finalProject/LinkedList.java
+++ src/finalProject/LinkedList.java
@@ -1,124 +1,82 @@
 package finalProject;
 
-public class LinkedList<T> {
-	
-	private int size;
-	private Node<T> head;
-	
-	public int size()
-	{
-		return size;
-	}
-	
-	public void add(T item)
-	{
-		 Node<T> node = new Node<T>(item);
-		 
-		 //If the list is empty, make this new node the first node
-	     if (this.head == null) 
-	     {
-	     	this.head = node;
-	     	this.size++;
-	     	return;
-	     }
-	     
-	     Node<T> currentNode = head;
-	     //Iterate until the last node in the list
-	     while(currentNode.next != null)
-	     {
-	    	 currentNode = currentNode.next;
-	     }
-	     
-	     //Add this new node to the end of the list
-	     currentNode.next = node;
-	     this.size++;
-	}
-	
-	public void add(int index, Node<T> node)
-	{
+public class LinkedList <T>{
+		private int size;
+		private Node<T> head;
 		
-	}
-	
-	public void addFirst(Node<T> node)
-	{
-		
-	}
-	
-	public void addLast(Node<T> node)
-	{
-		
-	}
-	
-	public boolean contains(Object o)
-	{
-		return false;
-	}
-	
-	public void remove()
-	{
-		
-	}
-	
-	public void remove(int index)
-	{
-		
-	}
-	
-	public void remove(Object o)
-	{
-		
-	}
-	
-	public void removeFirst()
-	{
-		
-	}
-	/**
-	public T get(int index)
-	{
-	
-	}
-	
-	public T getFirst()
-	{
-		
-	}
-	
-	public T getLast()
-	{
-	
-	}
-	*/
-	
-	public int indexOf(Object o)
-	{
-		return 0;
-	}
-	
-	
-	public void clear()
-	{
-		
-	}
-	
-	
-	
-	
-	
-	private static class Node<T>
-	{
-		private T value;
-		private Node<T> next;
-		
-		public Node(T value)
+		public int size()
 		{
-			this.value = value;
+			return 0;
 		}
 		
-		public T getValue()
+		public void add(Node<T> node)
 		{
-			return value;
+			
+		}
+		
+		public void remove(T value) {
+			if (this.head == null ) {
+				return;
+			}
+			if (this.head.value == value) {
+				this.head = this.head.next;
+				this.size--;
+				return;
+			}
+			
+			Node <T> currentNode = this.head;
+			while ( currentNode.next != null) {
+				if (currentNode.next.value == value) {
+				currentNode.next = currentNode.next.next;
+				this.size--;
+				return;
+			}
+			currentNode = currentNode.next;
+		}
+		}
+		
+		public T get(int index) {
+			if (this.size ==0 || index  > size -1) {
+				return null;
+			}
+			if (index == 0) {
+				return (T) this.head.value;
+			}
+			int currentIndex =0;
+			Node <T> currentNode = this.head;
+			
+			while ( currentNode.next != null) {
+				currentNode = currentNode.next;
+				if (++currentIndex == index) {
+					return (T) currentNode.value;
+				}
+			}
+			return null;
+		}
+		
+		public void clear()
+		{
+			
+		}
+		
+		
+		
+		
+		
+		private static class Node<T>
+		{
+			private T value;
+			private Node<T> next;
+			
+			public Node(T value)
+			{
+				this.value = value;
+			}
+			
+			public T getValue()
+			{
+				return value;
+			}
 		}
 	}
-}
 
